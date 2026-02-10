9d809d51e975d218f6bc31075d8348f3613d3823
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-02-10 11:00:37 +0200

    add link

--- shop/src/components/Home/ProductItem.js
+++ shop/src/components/Home/ProductItem.js
@@ -1,25 +1,29 @@
+import { Link } from "react-router-dom"
+
 export default function ProductItem({ item }) {
-    return (<div className="col-lg-4 col-md-6">
-        <div className="single-product">
-            <div className="product-img">
-                <img className="img-fluid w-100" src={item.img1} alt="" />
-                <div className="p_icon">
-                    <a href="#">
-                        <i className="ti-eye"></i>
-                    </a>
-                    <a href="#">
-                        <i className="ti-shopping-cart"></i>
-                    </a>
+    return (
+        <div className="col-lg-4 col-md-6">
+            <div className="single-product">
+                <div className="product-img">
+                    <img className="img-fluid w-100" src={item.img1} alt="" />
+                    <div className="p_icon">
+                        <Link to={`/product/${item.id}`}>
+                            <i className="ti-eye"></i>
+                        </Link>
+                        <a href="#">
+                            <i className="ti-shopping-cart"></i>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className="product-btm">
-                <a href="#" className="d-block">
-                    <h4>{item.productName}</h4>
-                </a>
-                <div className="mt-3">
-                    <span className="mr-4">€ {item.price}</span>
+                <div className="product-btm">
+                    <a href="#" className="d-block">
+                        <h4>{item.productName}</h4>
+                    </a>
+                    <div className="mt-3">
+                        <span className="mr-4">€ {item.price}</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>)
+    )
 }
